8b243cb2ff3e2dacbf70467ceb0e80ded7786d62
Shaheer Javed <shahirjavaid2000@gmail.com>
2022-11-29 18:34:46 +0500

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/artsy" 
+//     mongoURI: "mongodb://localhost:27017/artsy" 
+    mongoURI: "mongodb+srv://shaheer:shaheer123456789@cluster0.xgxdcjd.mongodb.net/fyp" 
   };
