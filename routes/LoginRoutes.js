2c2f17b8a9734640e596bb8422ac44d0e7acced2
Shaheer Javed <shahirjavaid2000@gmail.com>
2023-01-29 11:29:37 +0500

    updated logout

--- routes/LoginRoutes.js
+++ routes/LoginRoutes.js
@@ -66,6 +66,7 @@ router.post("/login", async (req, res) => {
 });
 
 router.get('/logout', (req, res) => {
+  req.session.destroy();
    res.status(200).json({ message: "Logged out successfully" });
 });
 
